cd8199a946e582f3006e069934a1e54c2439f36b
Your Name <rutaslv@hotmail.lt>
2020-08-13 21:57:39 +0300

    implement error page

--- errorpage.py
+++ errorpage.py
@@ -5,9 +5,12 @@ import constants
 class ErrorPage(tk.Frame):
 
     def __init__(self, parent, controller):
+        from startpage import StartPage
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Error page!!!", font=constants.LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        self.config(background="#ffeaa7")
+        label = tk.Label(self, text="Wrong name and/or ID.", bg="#ffeaa7", fg="#db0a63", font=constants.LARGE_FONT)
+        label.pack(pady=71, padx=175)
 
-        button = tk.Button(self, text="Exit", command=lambda: exit())
-        button.pack()
+        button = tk.Button(self, text="Back", bg="#55efc4", font=constants.LARGE_FONT,
+                           command=lambda: controller.show_frame(StartPage))
+        button.pack(pady=25)
