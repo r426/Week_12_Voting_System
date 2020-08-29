e757a89e77adfde304418a5457195a171d47bef4
Your Name <rutaslv@hotmail.lt>
2020-08-29 21:40:38 +0300

    show the list of parties and No. of votes

--- resultpage.py
+++ resultpage.py
@@ -1,13 +1,19 @@
 import tkinter as tk
 import constants
+import globals
 
 
 class ResultPage(tk.Frame):
 
     def __init__(self, parent, controller):
+        from startpage import StartPage
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Result page!!!", font=constants.LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        self.config(background="#ffeaa7")
+        for i in range(len(globals.parties)):
+            exec(
+                'tk.Label(self, text="%d. %s% d", bg="#ffeaa7", fg="#db0a63", font=constants.LARGE_FONT).pack(padx=175)' % (
+                    i + 1, globals.parties[i].name, globals.parties[i].votes))
 
-        button = tk.Button(self, text="Exit", command=lambda: exit())
-        button.pack()
+        button = tk.Button(self, text="Back", bg="#55efc4", font=constants.LARGE_FONT,
+                           command=lambda: controller.show_frame(StartPage))
+        button.pack(pady=25)
