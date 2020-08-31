9c363a6dc7e1ddef6bcca6f3c47a9ebc2234835c
Your Name <rutaslv@hotmail.lt>
2020-08-31 17:09:13 +0300

    update the result on the screen

--- resultpage.py
+++ resultpage.py
@@ -8,12 +8,17 @@ class ResultPage(tk.Frame):
     def __init__(self, parent, controller):
         from startpage import StartPage
         tk.Frame.__init__(self, parent)
+        self.update()
+        button1 = tk.Button(self, text="Update", bg="#55efc4", font=constants.LARGE_FONT,
+                            command=lambda: self.update())
+        button2 = tk.Button(self, text="Back", bg="#55efc4", font=constants.LARGE_FONT,
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack(pady=22)
+        button2.pack(pady=22)
+
+    def update(self):
         self.config(background="#ffeaa7")
         for i in range(len(globals.parties)):
-            exec(
-                'tk.Label(self, text="%d. %s% d", bg="#ffeaa7", fg="#db0a63", font=constants.LARGE_FONT).pack(padx=175)' % (
-                    i + 1, globals.parties[i].name, globals.parties[i].votes))
-
-        button = tk.Button(self, text="Back", bg="#55efc4", font=constants.LARGE_FONT,
-                           command=lambda: controller.show_frame(StartPage))
-        button.pack(pady=25)
+            lbl = 'tk.Label(self, text="%d. %s% d", bg="#ffeaa7", fg="#db0a63", font=constants.LARGE_FONT).pack(padx=230)' % (
+                i + 1, globals.parties[i].name, globals.parties[i].votes)
+            exec(lbl)
